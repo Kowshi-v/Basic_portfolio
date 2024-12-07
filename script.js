b95d8cb450be5e5378fd2acd65b4203e91606046
Kowshi-v <153633339+Kowshi-v@users.noreply.github.com>
2024-12-07 11:40:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show the popup
+function showPopup(type) {
+  document.getElementById(`popup-${type}`).style.display = "flex";
+}
+
+// Hide the popup
+function hidePopup(type) {
+  document.getElementById(`popup-${type}`).style.display = "none";
+}
